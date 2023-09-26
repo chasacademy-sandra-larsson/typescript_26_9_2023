33acfe1fc2df0da58f6a6c2e3af954561ee0e2dc
chasacademy-sandra-larsson <111293794+chasacademy-sandra-larsson@users.noreply.github.com>
2023-09-26 13:15:13 +0200

    Add files via upload

---
+++ test-in-js.js
@@ -0,0 +1,34 @@
+{
+
+    // Primitiver
+
+    let name = "Sandra"
+    let age = 23
+    let isCool = false
+    let height = undefined
+    let car = null
+
+
+    let bigInt = 12312123123123123131231;
+    let symbol = Symbol("symbol")
+
+
+    console.log(typeof name, name)
+    console.log(typeof age, age)
+    console.log(typeof isCool, isCool)
+    console.log(typeof height, height)
+    console.log(typeof car, car)
+    console.log(typeof bigInt, bigInt)
+    console.log(typeof symbol, symbol)
+
+    let pos = {x: 100, y: 100}
+
+    let arr = [1,2,3]
+
+    console.log(typeof pos)
+    console.log(typeof arr)
+
+    
+
+
+}
